0f3106a53ba8bfe85f002459abbd5bedf1ee5a14
van-gomes <nninha.gomes@gmail.com>
2025-06-19 18:57:10 -0300

    refactor components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+import { Header } from "./components/Header/Header";
 
 export function App() {
+  //Temporary placeholder for authentication status
+  const isAuthenticated = false;
+
   return (
     <BrowserRouter>
-      <Login />
-      {/* <Register /> */}
+      <Header isAuthenticated={isAuthenticated} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   );
 }
